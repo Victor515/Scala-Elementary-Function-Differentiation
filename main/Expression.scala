fbc198cb717ca8e3feb6b85fa68fb0ec30b8d0f3
victor515 <victoroyyb@gmail.com>
2018-10-02 10:35:01 -0500

    finish basic operations definition

--- main/Expression.scala
+++ main/Expression.scala
@@ -1,11 +1,21 @@
 package edu.rice.comp311.main
 
 abstract class Expression {
-  def *(that: Expression): Expression
-  def **(d: Double): Expression
-  def +(that: Expression): Expression
-  def -(that: Expression): Expression
-  def /(that: Expression): Expression
+  def *(that: Expression): Expression = {
+    Mutiply(this, that)
+  }
+  def **(d: Double): Expression = {
+    Power(this, Constant(d))
+  }
+  def +(that: Expression): Expression = {
+    Plus(this, that)
+  }
+  def -(that: Expression): Expression = {
+    Minus(this, that)
+  }
+  def /(that: Expression): Expression = {
+    Divide(this, that)
+  }
 
   def reduce(env: (String, Double)*): Double
 
@@ -18,7 +28,7 @@ abstract class Expression {
   override def toString: String = super.toString
 }
 
-case class Constant(n: Double) extends Expression
+case class Constant(d: Double) extends Expression
 
 case class Var(x: String) extends Expression
 
@@ -29,3 +39,13 @@ case class Exp(x: Expression) extends Expression
 case class Sin(x: Expression) extends Expression
 
 case class ArcSin(x: Expression) extends Expression
+
+case class Plus(first: Expression, second: Expression) extends Expression
+
+case class Minus(first: Expression, second: Expression) extends Expression
+
+case class Mutiply(first: Expression, second: Expression) extends Expression
+
+case class Divide(first: Expression, second: Expression) extends Expression
+
+case class Power(base: Expression, power: Constant) extends Expression
